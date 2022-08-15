e582539bb79d932433dce49b8f5b6e5154b01fdc
mariajoaosantos <mariajcs18@gmail.com>
2022-08-15 16:08:58 +0100

    Project

---
+++ script.js
@@ -0,0 +1,32 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+const vacation = "1 Nov 2022";
+
+function countdown() {
+  const vacationDate = new Date(vacation);
+  const currentDate = new Date();
+
+  const totalSeconds = (vacationDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds % 60);
+
+  daysEl.innerHTML = formatTime(days);
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+//initial call
+setInterval(countdown, 1000);
+
+countdown();
